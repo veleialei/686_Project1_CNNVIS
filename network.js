29050ad474eace451ac3e138ac1e575935f1d983
Shuangjiao Hu <shuangjiaohu@shuangjiaos-mbp.lan>
2018-10-25 22:51:57 -0700

    delete useless code

--- network.js
+++ network.js
@@ -1,5 +1,8 @@
-const loadInterval = 10;
-const visDivide = 30;
+const loadInterval = 40;
+const visDivide = 40;
+const opacityControl = 10;
+const widthMinControl = 3;
+const widthMaxControl = 12;
 
 const nodeSize  = 30;
 const height = 630;
@@ -80,25 +83,19 @@ function loadData(batchId) {
     imgElt.src = "cifar10/cifar10_batch_" + batchId + ".png";
 }
 
-//their code
 function trainingInstance() {
-    let bi = Math.floor(Math.random() * loadedBatches.length);
-    let b = loadedBatches[bi];
-    let k = Math.floor(Math.random() * samplesPerBatch); // sample within the batch
-    let n = b * samplesPerBatch + k;
+    let randomedBatch = loadedBatches[Math.floor(Math.random() * loadedBatches.length)];
+    let k = Math.floor(Math.random() * samplesPerBatch);
+    let n = randomedBatch * samplesPerBatch + k;
     let isval = n % 10 === 0 ? true : false;
-
-    let p = imgData[b].data;
     let x = new convnetjs.Vol(imageDim, imageDim, imageChannels, 0.0);
-    let W = imageDim * imageDim;
 
-    // fetch the appropriate row of the training image and reshape into a Vol
     for (let dc = 0; dc < imageChannels; dc++) {
         let i = 0;
         for (let xc = 0; xc < imageDim; xc++) {
             for (let yc = 0; yc < imageDim; yc++) {
-                let ix = ((W * k) + i) * 4 + dc;
-                x.set(yc, xc, dc, p[ix] / 255.0 - 0.5);
+                let ix = ((imageDim * imageDim * k) + i) * 4 + dc;
+                x.set(yc, xc, dc, imgData[randomedBatch].data[ix] / 255.0 - 0.5);
                 i++;
             }
         }
@@ -169,179 +166,166 @@ function graphInstance() {
     }
 }
 
-//class
-function CNNVis(canvas, data){
-	let _self = this;
-	_self.data = data;
-	console.log(data);
-	_self.jqueryCanvas = $(canvas);
-	_self.canvas = d3.select(canvas);
-  d3.select(canvas).style("position", "relative");
+class CNNVis {
+  constructor(canvas, data){
+  	this.data = data;
+  	console.log(data);
+  	this.jqueryCanvas = $(canvas);
+  	this.canvas = d3.select(canvas);
+  	this.nodeGraph = [];
+  	d3.select(canvas).style("position", "relative");
+  	this.svg = this.canvas.append("svg")
+  		.attr("width", width)
+  		.attr("height", height)
+  		.append("g")
+  		.attr("class", "svg_path");
 
-	_self.nodeGraph = [];
-	_self.svg = _self.canvas.append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.append("g")
-		.attr("class", "svg_path");
-
-	_self.drawLayers();
-	_self.drawPaths();
-	_self.drawNode();
-}
-
-CNNVis.prototype.drawLayers = function(){
-	let _self = this;
-
-	const layerData = _self.data["nodeImages"].slice(1, _self.data["nodeImages"].length-1);
-	const beginNode = _self.canvas.selectAll("div")
-		.data(_self.data["nodeImages"][0])
-		.enter()
-		.append("div")
-		.attr("id", "beginNode")
-		.style({width: nodeSize,
-						height: nodeSize,
-						"vertical-align": "middle",
-						position: "absolute",
-						top: function(d){return (height/2-d3.select(d).attr("height")/2)+"px";},
-						left: function(d){return (nodeSize-d3.select(d).attr("width")/2)+"px"; }});
-	beginNode.append(function(d){return d;});
-
-	const endData = _self.data["nodeImages"][_self.data["nodeImages"].length-1];
-	const endNode = _self.canvas.selectAll("div.endNode")
-		.data(endData).enter()
-		.append("div")
-		.attr("class", "endNode")
-		.style({width: nodeSize,
-						height: nodeSize,
-						"vertical-align": "middle",
-						position: "absolute",
-						top: function(d,i){return (i*nodeSize*2 + (height/2 - nodeSize*2*endData.length/2)-d3.select(d).attr("height")/2 )+"px";},
-						right: function(d,i){return (nodeSize*2-d3.select(d).attr("width")/2)+"px";}});
-	endNode.append(function(d){return d;});
-
-	const layerDiv = _self.canvas.selectAll("div.layers")
-		.data(layerData).enter()
-		.append("div")
-		.attr("class", "layers")
-		.attr("id", function(d,i){return "layer_"+(i+1);})
-		.style({width: nodeSize,
-			height: nodeSize,
-			"vertical-align": "middle",
-			position: "absolute",
-			left: function(d,i){return (i+1)*unitWidth+"px";}});
-
-	const filterDiv = layerDiv.selectAll("div.filters")
-		.data(function(d){return d;}).enter()
-		.append("div")
-		.attr("class", "filters")
-		.attr("id", function(d,i){return "filter_"+i;})
-		.style({width: nodeSize,
-			height: nodeSize,
-			"vertical-align": "middle",
-			position: "absolute",
-			top: function(d,i){return (i*nodeSize*2-d3.select(d).attr("width")/2)+"px";},
-			left: function(d){return (nodeSize*4-d3.select(d).attr("height")/2)+"px"}});
-
-	_self.nodeGraph = filterDiv.append("div");
-}
-
-CNNVis.prototype.drawPaths = function(){
-	let _self = this;
-
-	const line = d3.svg.line().x(function(d) { return d.x;}).y(function(d) { return d.y;});
-  const pathsData = [];
-  const pathControlDomain = {};
-
-  _self.data["pathIntensity"].forEach(function(layer, layerIndex){
-    	layer.forEach(function(filter, filterIndex){
-    		let obj = {}
-				if(layerIndex != 0){
-					obj["x"] = unitWidth*layerIndex + nodeSize*4;
-    			obj["y"] = filterIndex*nodeSize*2;
-    		}else{
-					obj["x"] = nodeSize;
-					obj["y"] = height/2;
-    		}
-    		filter.forEach(function(path, pathIndex){
-    			let pathObject = {};
-          //line x, y
-    			if(layerIndex==0){
-    				pathObject["x"] = unitWidth*(layerIndex+1) + nodeSize*4;
-    				pathObject["y"] = pathIndex*nodeSize*2;
-    			}else if(layerIndex == _self.data["pathIntensity"].length-2){
-    				pathObject["x"] = width-nodeSize;
-    				pathObject["y"] = (pathIndex*nodeSize*2 + (height/2 - nodeSize*2*filter.length/2));
-    			}else{
-    				pathObject["x"] = unitWidth*(layerIndex+1) + nodeSize*4;
-    				pathObject["y"] = pathIndex*nodeSize*2;
-    			}
-
-          //line intensity
-    			if(layerIndex != _self.data["pathIntensity"].length-1){
-    				obj["layer"] = layerIndex;
-    				pathObject["layer"] = layerIndex;
-    				if(pathControlDomain[layerIndex]==undefined){
-    					pathControlDomain[layerIndex]=[path];
-    				}else{
-    					pathControlDomain[layerIndex].push(path)
-    				}
-    				obj["pathIntensity"] = path;
-    				pathObject["pathIntensity"] = path;
-    				pathsData.push([obj, pathObject]);
-    			}
-    		})
-    	})
-    });
-
-  let pathWidthControl = {};
-  for(let key in pathControlDomain){
-    let scale = d3.scale.linear().domain(d3.extent(pathControlDomain[key])).range([5, 25]);
-    pathWidthControl[key] = scale;
+  	this.drawLayers();
+  	this.drawPaths();
+  	this.drawNode();
   }
-    _self.svg.selectAll("path")
-    	.data(pathsData).enter()
-    	.append("path")
-    	.attr("d", line)
-      .style("stroke","#FFA500")
-			.style("stroke-width", function(d){
-      	return 0.8*pathWidthControl[d[0]["layer"]](d[0]["pathIntensity"]);
-      })
-    	.style("opacity", function(d){
-    		let intensityScale = pathControlDomain[d[0]["layer"]].sort();
-    		if(d[0]["layer"]==0){
-    			return 0.4;
-    		}
-    		if(intensityScale.indexOf(d[0]["pathIntensity"]) < intensityScale.length/2){
-    			return 0;
-    		}else{
-    			return 0.4;
-    		}
-    	});
-}
 
-CNNVis.prototype.drawNode = function(){
-	let _self = this;
-	let nodes = [];
-	_self.nodeGraph.each(function(d){
-		nodes.push(d3.select(this).append(function(d){return d;}));
-	});
-}
+  drawLayers(){
+  	const layerData = this.data["nodeImages"].slice(1, this.data["nodeImages"].length-1);
+    //each layer
+  	const layerDiv = this.canvas.selectAll("div.layers")
+  		.data(layerData).enter()
+  		.append("div")
+  		.attr("class", "layers")
+  		.attr("id", function(d,i){return "layer_"+(i+1);})
+  		.style({
+  			position: "absolute",
+  			left: function(d,i){return (i+1)*unitWidth+"px";}});
 
-CNNVis.prototype.removeGraphs = function(){
-	let _self = this;
-	_self.canvas.selectAll("div#beginNode").remove();
-	_self.canvas.selectAll("div.endNode").remove();
-	_self.canvas.selectAll("div.layers").remove();
-  _self.svg.selectAll("path").remove();
-}
+    //each node on layer
+  	const filterDiv = layerDiv.selectAll("div.filters")
+  		.data(function(d){return d;}).enter()
+  		.append("div")
+  		.attr("class", "filters")
+  		.attr("id", function(d,i){return "filter_"+i;})
+  		.style({
+  			position: "absolute",
+  			top: function(d,i){return (i*nodeSize*2-d3.select(d).attr("width")/2)+"px";},
+  			left: function(d){return (nodeSize*4-d3.select(d).attr("height")/2)+"px"}});
 
-CNNVis.prototype.update = function(data){
-	let _self = this;
-	_self.data = data;
-	_self.removeGraphs();
-	_self.drawLayers();
-	$('div.layers').prependTo(_self.jqueryCanvas);
-	_self.drawNode();
-	_self.drawPaths();
+  	this.nodeGraph = filterDiv.append("div");
+
+    //draw the input layer
+    const inputLayer = this.canvas.selectAll("div")
+  		.data(this.data["nodeImages"][0])
+  		.enter()
+  		.append("div")
+  		.attr("id", "inputLayer")
+  		.style({position: "absolute",
+  						top: function(d){return (height/2-d3.select(d).attr("height")/2)+"px";},
+  						left: function(d){return (nodeSize-d3.select(d).attr("width")/2)+"px"; }});
+  	inputLayer.append(function(d){return d;});
+
+    //draw the output layer
+  	const outputData = this.data["nodeImages"][this.data["nodeImages"].length-1];
+  	const outputNode = this.canvas.selectAll("div.outputNode")
+  		.data(outputData).enter()
+  		.append("div")
+  		.attr("class", "outputNode")
+  		.style({position: "absolute",
+  						top: function(d,i){return (i*nodeSize*2 + (height/2 - nodeSize*2*outputData.length/2)-d3.select(d).attr("height")/2 )+"px";},
+  						right: function(d,i){return (nodeSize*2-d3.select(d).attr("width")/2)+"px";}});
+  	outputNode.append(function(d){return d;});
+
+
+  }
+
+  drawPaths(){
+  	const line = d3.svg.line().x(function(d) { return d.x;}).y(function(d) { return d.y;});
+    const pathsData = [];
+    const pathWidthControl = {};
+    const pathControlDomain = {};
+    const pathInensityLength = this.data["pathIntensity"].length;
+
+    this.data["pathIntensity"].forEach(function(layer, layerIndex){
+      	layer.forEach(function(filter, filterIndex){
+      		let obj = {}
+  				if(layerIndex != 0){
+  					obj["x"] = unitWidth*layerIndex + nodeSize*4;
+      			obj["y"] = filterIndex*nodeSize*2;
+      		}else{
+  					obj["x"] = nodeSize;
+  					obj["y"] = height/2;
+      		}
+      		filter.forEach(function(path, pathIndex){
+      			let pathObject = {};
+            //line x, y
+      			if(layerIndex==0){
+      				pathObject["x"] = unitWidth*(layerIndex+1) + nodeSize*4;
+      				pathObject["y"] = pathIndex*nodeSize*2;
+      			}else if(layerIndex == pathInensityLength-2){
+      				pathObject["x"] = width-nodeSize;
+      				pathObject["y"] = (pathIndex*nodeSize*2 + (height/2 - nodeSize*2*filter.length/2));
+      			}else{
+      				pathObject["x"] = unitWidth*(layerIndex+1) + nodeSize*4;
+      				pathObject["y"] = pathIndex*nodeSize*2;
+      			}
+
+            //line intensity
+      			if(layerIndex != pathInensityLength-1){
+      				obj["layer"] = layerIndex;
+      				pathObject["layer"] = layerIndex;
+      				if(pathControlDomain[layerIndex]==undefined){
+      					pathControlDomain[layerIndex]=[path];
+      				}else{
+      					pathControlDomain[layerIndex].push(path)
+      				}
+      				obj["pathIntensity"] = path;
+      				pathObject["pathIntensity"] = path;
+      				pathsData.push([obj, pathObject]);
+      			}
+      		})
+      	})
+      });
+
+    for(let key in pathControlDomain){
+      let scale = d3.scale.linear().domain(d3.extent(pathControlDomain[key])).range([widthMinControl, widthMaxControl]);
+      pathWidthControl[key] = scale;
+    }
+      this.svg.selectAll("path")
+      	.data(pathsData).enter()
+      	.append("path")
+      	.attr("d", line)
+        .style("stroke","orange")
+  			.style("stroke-width", function(d){
+        	return pathWidthControl[d[0]["layer"]](d[0]["pathIntensity"]);
+        })
+      	.style("opacity", function(d){
+          // return pathWidthControl[d[0]["layer"]](d[0]["pathIntensity"])/12;
+      		let intensityScale = pathControlDomain[d[0]["layer"]].sort();
+      		if(d[0]["layer"]==0) return 0.4;
+      		if(intensityScale.indexOf(d[0]["pathIntensity"]) < intensityScale.length/opacityControl){
+      			return 0;
+      		}
+      		return 0.4;
+      	});
+  }
+
+  drawNode (){
+  	let nodes = [];
+  	this.nodeGraph.each(function(d){
+  		nodes.push(d3.select(this).append(function(d){return d;}));
+  	});
+  }
+
+  removeGraphs (){
+    this.canvas.selectAll("div#inputLayer").remove();
+  	this.canvas.selectAll("div.outputNode").remove();
+  	this.canvas.selectAll("div.layers").remove();
+    this.svg.selectAll("path").remove();
+  }
+
+  update (data){
+  	this.data = data;
+  	this.removeGraphs();
+  	this.drawLayers();
+  	$('div.layers').prependTo(this.jqueryCanvas);
+  	this.drawNode();
+  	this.drawPaths();
+  }
 }
